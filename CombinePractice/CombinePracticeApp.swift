fd8745148c6d1cd2b2d46bdadd89a3cff08b5e28
koro <koro.nn99@gmail.com>
2021-04-03 02:47:36 -0700

    Initial Commit

---
+++ CombinePractice/CombinePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePracticeApp.swift
+//  CombinePractice
+//
+//  Created by Koro Saka on 2021-04-03.
+//
+
+import SwiftUI
+
+@main
+struct CombinePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombinePractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombinePractice
+//
+//  Created by Koro Saka on 2021-04-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
