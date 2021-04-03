c563cdc07482ffabe926340dd48d90e2557166c3
koro <koro.nn99@gmail.com>
2021-04-03 04:17:45 -0700

    test assign & map

--- CombinePractice/DoingSomething.swift
+++ CombinePractice/DoingSomething.swift
@@ -11,6 +11,9 @@ import Combine
 class DoingSomething {
     
     init() {
+        
+        //MARK: 1. Just
+        
         //MARK: original
         let publisher = Just(100)
         
@@ -46,5 +49,26 @@ class DoingSomething {
         let cancelable2 = Just(777).sink(receiveValue: { value in
             print("value :\(value)")
         })
+        
+        //MARK: 2. assign & map
+        let testResult = TestResult(score: "0")
+        
+        let cancelable3 = Just(100)
+            .map({ value in
+                return String(value)
+            })
+            .assign(to: \.score, on: testResult)
+        
+        print("test score: \(testResult.score)")
+        
     }
+    
+    class TestResult {
+        var score: String
+        init(score: String) {
+            self.score = score
+        }
+    }
+    
+    
 }
